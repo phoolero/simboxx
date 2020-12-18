1372c54529816ff79b83492719298eeae39b33d9
alfredo <aalfredoplz@gmail.com>
2020-12-18 17:18:17 -0300

    form de usuario para editar

---
+++ src/Form/UsuarioType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+class UsuarioType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre', TextType::class, array(
+                'label' => 'Nombre'
+            ))
+            ->add('email', EmailType::class, array(
+                'label' => 'Email'
+            ))
+            ->add('submit', SubmitType::class, array(
+                'label' => 'Actualizar'
+            ))
+        ;
+    }
+
+
+}
