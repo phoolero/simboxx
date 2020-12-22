c4b56a80c7a33d632a4a9e873c80b2f351a3d3da
alfredo <aalfredoplz@gmail.com>
2020-12-22 08:40:11 -0300

    nuevo metodo new para almacenar archivos de img para carnes

--- src/Controller/CedulaController.php
+++ src/Controller/CedulaController.php
@@ -9,7 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
 /**
  * @Route("/cedula")
  */
@@ -28,7 +30,7 @@ class CedulaController extends AbstractController
     /**
      * @Route("/new", name="cedula_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, SluggerInterface $slugger): Response
     {
         $cedula = new Cedula();
         $form = $this->createForm(CedulaType::class, $cedula);
@@ -36,6 +38,30 @@ class CedulaController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
+            $brochureFile = $form->get('ruta_imagen')->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$brochureFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('archivo_img_carnet'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    throw new \Exception( 'Hubo un error en la subida de archivo');
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $cedula->setRutaImagen($newFilename);
+            }
             $entityManager->persist($cedula);
             $entityManager->flush();
 
@@ -47,7 +73,7 @@ class CedulaController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
-
+    
     /**
      * @Route("/{id}", name="cedula_show", methods={"GET"})
      */
@@ -83,7 +109,7 @@ class CedulaController extends AbstractController
      */
     public function delete(Request $request, Cedula $cedula): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$cedula->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete'.$cedula->getRut(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($cedula);
             $entityManager->flush();
