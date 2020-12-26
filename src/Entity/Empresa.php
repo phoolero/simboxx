7c0cab316a209e142e900d080050b2fdf3f0cc8b
alfredo <aalfredoplz@gmail.com>
2020-12-26 09:00:54 -0300

    empresa entity

---
+++ src/Entity/Empresa.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmpresaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EmpresaRepository::class)
+ */
+class Empresa
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\OneToMany(targetEntity="App\Entity\Alumno", mappedBy="institucion")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     */
+    private $rut;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $fecha_inicio;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $fecha_fin;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getRut(): ?string
+    {
+        return $this->rut;
+    }
+
+    public function setRut(?string $rut): self
+    {
+        $this->rut = $rut;
+
+        return $this;
+    }
+
+    public function getFechaInicio(): ?\DateTimeInterface
+    {
+        return $this->fecha_inicio;
+    }
+
+    public function setFechaInicio(?\DateTimeInterface $fecha_inicio): self
+    {
+        $this->fecha_inicio = $fecha_inicio;
+
+        return $this;
+    }
+
+    public function getFechaFin(): ?\DateTimeInterface
+    {
+        return $this->fecha_fin;
+    }
+
+    public function setFechaFin(?\DateTimeInterface $fecha_fin): self
+    {
+        $this->fecha_fin = $fecha_fin;
+
+        return $this;
+    }
+}
