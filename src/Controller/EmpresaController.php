6fe706528c71190935eb60b00341a1b25f4d2586
alfredo <aalfredoplz@gmail.com>
2020-12-27 20:54:57 -0300

    rutas y metodos crud controller empresa

--- src/Controller/EmpresaController.php
+++ src/Controller/EmpresaController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Empresa;
 use App\Form\EmpresaType;
 use App\Repository\EmpresaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+
 
 
 class EmpresaController extends AbstractController
@@ -33,13 +35,46 @@ class EmpresaController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($empresa);
             $entityManager->flush();
-
-            return $this->redirectToRoute('cheque_index');
+        
+            return $this->redirectToRoute('empresas');
         }
+        
 
-        return $this->render('cheque/new.html.twig', [
-            'cheque' => $cheque,
+        return $this->render('empresa/new.html.twig', [
+            'empresa' => $empresa,
             'form' => $form->createView(),
         ]);
+    }
+    public function show(Empresa $empresa): Response
+    {
+        return $this->render('empresa/show.html.twig', [
+            'empresa' => $empresa,
+        ]);
+    }
+    public function edit(Request $request, Empresa $empresa): Response
+    {
+        $form = $this->createForm(EmpresaType::class, $empresa);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('empresas');
+        }
+
+        return $this->render('empresa/edit.html.twig', [
+            'empresa' => $empresa,
+            'form' => $form->createView(),
+        ]);
+    }
+    public function delete(Request $request, Empresa $empresa): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$empresa->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($empresa);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('empresas');
     }
 }
